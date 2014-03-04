964bc6300e044240a3cd44c89c3aea646662983e
Hariadi Hinta <diperakui@yahoo.com>
2014-03-04 20:46:58 +0800

    use grunt-readme

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+/*
+ * generator-plugin
+ * https://github.com/assemble/generator-plugin
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    jshint: {
+      all: ['Gruntfile.js', 'app/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-readme');
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'readme']);
+
+};
---
+++ docs/docs-related.md
@@ -0,0 +1,3 @@
+
+ * [Assemble generator](https://github.com/assemble/generator-assemble)
+ * [Assemble helper generator](https://github.com/assemble/generator-helper)
